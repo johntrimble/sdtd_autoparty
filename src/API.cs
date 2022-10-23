f7b049367044052943308803d6074559b78f9f6e
John Trimble <trimblej@gmail.com>
2022-10-23 14:11:38 -0700

    Fix bug that led to disjoint parties

--- src/API.cs
+++ src/API.cs
@@ -13,6 +13,53 @@ namespace AutoParty {
             return entityPlayer;
         }
 
+        private static bool ServerHasParty() {
+            return GameManager.Instance.World.GetPlayers().Find( (EntityPlayer player) => player.IsInParty() ) != null;
+        }
+
+        private static List<EntityPlayer> GetOtherPlayers(EntityPlayer player) {
+            List<EntityPlayer> players = GameManager.Instance.World.GetPlayers();
+            if (player != null) {
+                players = players.FindAll( (EntityPlayer other) => other.entityId != player.entityId );
+            }
+            return players;
+        }
+
+        private static List<EntityPlayer> GetFriendsOfPlayer(EntityPlayer player) {
+            return GetOtherPlayers(player).FindAll( (EntityPlayer other) => other.IsFriendsWith(player) );
+        }
+
+        private static EntityPlayer GetFirstPlayerThatIsPartyLead(List<EntityPlayer> players) {
+            return players.Find( (EntityPlayer other) => other.IsInParty() && other.IsPartyLead() && other.Party != null );
+        }
+
+        private static void AddToFriendsParty(EntityPlayer player) {
+            if (player.IsInParty()) {
+                return;
+            }
+
+            Log.Out(string.Format("[AUTOPARTY] Player {0} is not in party, looking for party to join", player.EntityName));
+            List<EntityPlayer> friends = GetFriendsOfPlayer(player);
+            EntityPlayer partyLead = GetFirstPlayerThatIsPartyLead(friends);
+            if (partyLead != null) {
+                Log.Out(string.Format("[AUTOPARTY] Player {0} is freinds with {1}, joining their party", player.EntityName, partyLead.EntityName));
+                Party.ServerHandleAcceptInvite(partyLead, player);
+                return;
+            } else if (ServerHasParty()) {
+                Log.Out(string.Format("[AUTOPARTY] Could not find allied party for {0}, not creating new party as parties already exist", player.EntityName));
+                return;
+            } else {
+                partyLead = friends.Find((EntityPlayer other) => !other.IsInParty());
+                if (partyLead != null) {
+                    Log.Out(string.Format("[AUTOPARTY] Player {0} is freinds with {1}, creating new party", player.EntityName, partyLead.EntityName));
+                    Party.ServerHandleAcceptInvite(partyLead, player);
+                    return;
+                }
+            }
+
+            Log.Out(string.Format("[AUTOPARTY] Could not find allied party for {0}", player.EntityName));
+        }
+
         private static void PlayerSpawned(ClientInfo info, RespawnType t, Vector3i pos) {
             Log.Out(string.Format("[AUTOPARTY] Handler fired, respawn type {0}", t));
             
@@ -20,29 +67,7 @@ namespace AutoParty {
                 Log.Out(string.Format("[AUTOPARTY] Player spawned in {0}", info.playerName));
 
                 EntityPlayer player = GetEntityPlayer(info.entityId);
-                if (player.IsInParty()) {
-                    return;
-                }
-
-                Log.Out(string.Format("[AUTOPARTY] Player {0} is not in party, looking for party to join", player.EntityName));
-                foreach (KeyValuePair<int, EntityPlayer> entry in GameManager.Instance.World.Players.dict ) {
-                    EntityPlayer other = entry.Value;
-                    if (other.entityId == player.entityId) {
-                        continue;
-                    }
-
-                    if (player.partyInvites != null && player.partyInvites.Contains(other)) {
-                        continue;
-                    }
-
-                    if ( (other.IsFriendsWith(player) && other.IsInParty() && other.IsPartyLead() && other.Party != null) || !other.IsInParty() ) {
-                        Log.Out(string.Format("[AUTOPARTY] Player {0} is freinds with {1}, joining their party", player.EntityName, other.EntityName));
-                        Party.ServerHandleAcceptInvite(other, player);
-                        return;
-                    }
-                }
-
-                Log.Out(string.Format("[AUTOPARTY] Could not find allied party for {0}", player.EntityName));
+                AddToFriendsParty(player);
             }
         }
     }
